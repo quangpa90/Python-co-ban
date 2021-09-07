7623cc36e713d1e38f379b6b06df15d8b234c5b2
QuangPa <phamanhquangit@gmail.com>
2021-09-07 13:11:03 +0700

    In ra màn hình ký tự tại index

--- Less2.py
+++ Less2.py
@@ -3,13 +3,13 @@
         If the index is out of range, the program should print "i is out of range"
         If the string is empty, the program should print ""
 '''
-
-s = "Pizza"
-i = int(input("Nhap vao index:"))
-if s == "":
-    print("Empty String")
-elif i >= len(s):
-    print("i is out of range")
-else:
-    print("ky tu tai index i la: {}".format(s[i]))
+if __name__ == "__main__":
+    s = "Pizza"
+    i = int(input("Nhap vao index:"))
+    if s == "":
+        print("Empty String")
+    elif i >= len(s):
+        print("i is out of range")
+    else:
+        print("ky tu tai index i la: {}".format(s[i]))
 
