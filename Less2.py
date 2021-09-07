53af77ba96124fb4d54e0992c2863d902b97d29e
QuangPa <phamanhquangit@gmail.com>
2021-09-07 12:45:25 +0700

    In ra màn hình index i trong string s

--- Less2.py
+++ Less2.py
@@ -1,14 +1,15 @@
 '''
- bài 2: yêu cầu viết chương trình khi nhập 1 index trong string s in ra ký tự tại vị trí đã nhập
-    nếu string rỗng in ra màn hình "Empty string"
-    nếu index ngoài string đã cho thì in ra "i is out of range"*//
+         Write a Python program that prints the character at index i in the string s.
+        If the index is out of range, the program should print "i is out of range"
+        If the string is empty, the program should print ""
 '''
 
-s = "Hello"
-n = int(input("Nhập vị trí cần lấy trong chuỗi"))
+s = "Pizza"
+i = int(input("Nhap vao index:"))
 if s == "":
-    print("Empty string")
-elif n >= len(s):
+    print("Empty String")
+elif i >= len(s):
     print("i is out of range")
 else:
-    print("ký tự tại index {} là:{}".format(n,s[n]))
+    print("ky tu tai index i la: {}".format(s[i]))
+
