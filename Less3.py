1264b4f4477e36837c7d38e74de042303123cb3b
QuangPa <phamanhquangit@gmail.com>
2021-09-07 13:24:47 +0700

    Đảo ngược chuỗi

--- Less3.py
+++ Less3.py
@@ -3,14 +3,14 @@ Write a Python Program that prints the reversed version of a string.
 The program must preserve uppercase and lowercase letters.
 If the string is empty, print it intact.
 """
-# cách 1
-s = "Wo"
-print("cách 1: {}".format(s[::-1]))
-# s[start:stop:step]
-
-# cách 2:
-reverse_str = "".join(reversed(s))
-print("cách 2: {}".format(reverse_str))
-
-# reversed(s) =>> tạo ra 1 list ['o', 'W'] list này đã đảo ngược
-# tiếp theo dùng hàm join() nó sẽ tạo ra string mới đã đảo ngược
+if __name__== "__main__":
+    # cách 1:
+    s = "Hello"
+    print("cách 1: {}".format(s[::-1]))
+    # Cách 1 dùng s[start:stop:step]
+    # cách 2:
+    reverse_str = "".join(reversed(s))
+    print("cách 2: {}".format(reverse_str))
+    # Cách 2 dùng hàm reversed()
+    # reversed(s) hàm này tạo ra list đảo ngược =>> ['o','l','l','e','H']
+    # tiếp đó dùng hàm join() join list thành chuỗi tạo ra chuỗi mới 0lleH
